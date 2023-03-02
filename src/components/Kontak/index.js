1cb436dace235b959fca3e5cb95a8a4580221520
gilangramadhahatno <gilangramadhanhatno@gmail.com>
2023-03-02 15:17:37 +0700

    addes component kontak

---
+++ src/components/Kontak/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import map from "../../assets/kontak/map-kontak.png";
+
+import "./index.css";
+
+export default function Kontak() {
+  return (
+    <sectio className="kontak">
+      <img src={map} alt="map-kontak" />
+    </sectio>
+  );
+}
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../components/Header/index";
+import Kontak from "../components/Kontak/index";
 import LayananKami from "../components/LayananKami/index";
 import TentangKami from "../components/TentangKami";
 
@@ -9,6 +10,7 @@ export default function LandingPage() {
       <Header />
       <LayananKami />
       <TentangKami />
+      <Kontak />  
     </>
   );
 }
